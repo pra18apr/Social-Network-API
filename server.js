51c919af99f2352fa22d9ba86190f71c460e3f61
pra18apr <pra.18apr@gmail.com>
2023-09-18 15:12:22 -0600

    added server.js file

---
+++ server.js
@@ -0,0 +1,29 @@
+// Server for NoSQL Social API
+// Dependencies
+// Express.js
+const express = require('express');
+// Mongoose ORM for MongoDB
+const mongoose = require('mongoose');
+
+// Initialize the server
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Middleware for JSON and string parsing
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// Middleware for the API routes
+app.use(require('./routes'));
+
+// Mongoose database connection
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/nosql-social', {
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+// Log the mongo queries being executed
+mongoose.set('debug', true);
+
+// Open a port for the server
+app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
