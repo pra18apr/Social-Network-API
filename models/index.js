f71a66fce79799ac34c444480d367bcbdc8506f7
pra18apr <pra.18apr@gmail.com>
2023-09-18 15:16:45 -0600

    added index.js file to project

---
+++ models/index.js
@@ -0,0 +1,10 @@
+// Model Index
+
+// User Model
+const User = require('./User');
+
+// Thought Model
+const Thought = require('./Thought');
+
+// Export the models
+module.exports = { User, Thought }
